62624f3272c7506387e5fb68ce8fabb8d9a398dc
Cloud Park <vividxenon@gmail.com>
2018-06-12 18:08:46 +0900

    flask_bot 챗봇기본뼈대

--- flask_bot.py
+++ flask_bot.py
@@ -12,7 +12,7 @@ def Keyboard():
 
     dataSend = {
         "type" : "buttons",
-        "buttons" : ["시작하기", "도움말"]
+        "buttons" : ["관객수 그래프 보기", "현재상영작 보기", "개봉예정작 보기", "장르별 현재상영작", "평점순 현재상영작"]
     }
 
     return jsonify(dataSend)
@@ -26,28 +26,51 @@ def Message():
     content = dataReceive['content']
     print()
 
-    if content == u"시작하기":
+    if content == u"관객수 그래프 보기":
         dataSend = {
             "message": {
-                "text": "아직 개발중이라 대답을 잘 못해도 이해해줘^^;"
+                "text": "현아야 일해라"
             }
         }
-    elif content == u"도움말":
+    elif content == u"현재상영작 보기":
         dataSend = {
             "message": {
-                "text": "이제 곧 정식 버전이 출시될거야. 조금만 기다려~~~"
+                "text": "현재상영작 보기"
             }
         }
-    elif u"안녕" in content:
+
+    elif content == u"개봉예정작 보기":
         dataSend = {
             "message": {
-                "text": "안녕~~ 반가워 ㅎㅎ"
+                "text": "개봉예정작 보기"
+            }
+        }
+
+    elif content == u"장르별 현재상영작":
+        dataSend = {
+            "message": {
+                "text": "장르별 현재상영작"
+            }
+        }
+
+    elif content == u"평점순 현재상영작:
+        dataSend = {
+            "message": {
+                "text": "장르별 현재상영작"
+            }
+        }
+
+
+    elif u"누구냐" in content:
+        dataSend = {
+            "message": {
+                "text": "뭠마"
             }
         }
     else:
         dataSend = {
             "message": {
-                "text": "나랑 놀자 ㅋㅋㅋ"
+                "text": "죄송합니다ㅠ 문송해서 인식할수가 없습니다 ㅋㅋㅋ"
             }
         }
 
